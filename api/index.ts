23543d921116a7c2eb7b21e19a481bafe8fdae59
Røkshan <33148219+R0kshan@users.noreply.github.com>
2026-05-02 17:54:44 +0200

    Added error message if username is not specified

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,14 @@ const { generateConstellation } = require('../lib/generateConstellation')
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 
-    const { username = 'R0kshan', terminalColor = '#96C7FF' } = req.query
+    const { username = '', terminalColor = '#96C7FF' } = req.query
+
+    if (!username) {
+        return res.status(400).json({
+            message: 'Username query parameter is required'
+        });
+    }
+
     const svg = await generateConstellation(username as string, terminalColor as string)
 
     res.setHeader('Content-Type', 'image/svg+xml')
