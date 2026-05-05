d0d6cb74e8a2fe2bbc4316ae3c5b2db13cd03ae7
R0kshan <33148219+R0kshan@users.noreply.github.com>
2026-05-05 23:02:05 +0200

    Convert the constellation generation script to Typescript

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-const { generateConstellation } = require('../lib/generateConstellation')
+import { generateConstellation } from '../lib/generateConstellation'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 
